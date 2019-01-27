28849350e181c58cbdd178958f91fcb23533d46c
harrisonodi <odiharrison@gmail.com>
2019-01-27 19:34:12 +0000

    Initial product test

---
+++ Amanson/StepDefinations/ProductDetailSteps.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Amanson.StepDefinations
+{
+    [Binding]
+    public sealed class ProductDetailSteps
+    {
+        IWebDriver driver;
+        [Given(@"I navigate to Amazon homepage")]
+        public void GivenINavigateToAmazonHomepage()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.amazon.co.uk/"); 
+        }
+
+        [Given(@"I have search for wrist watch")]
+        public void GivenIHaveSearchForWristWatch()
+        {
+                    }
+
+        [Given(@"I click one of the search result displayed")]
+        public void GivenIClickOneOfTheSearchResultDisplayed()
+        {
+            
+        }
+
+        [Then(@"The product price is displayed")]
+        public void ThenTheProductPriceIsDisplayed()
+        {
+            
+        }
+
+    }
+
+
+}
---
+++ Amanson/features/CustomerCanViewProductDetail.feature
@@ -0,0 +1,14 @@
+﻿Feature: CustomerCanViewProductDetail
+	In order to know more about a product
+	As a customer
+	I want to be  anle to view the details of the product
+
+
+Scenario:  Customer can view product price
+	Given I navigate to Amazon homepage
+	And I have search for wrist watch
+	And  I click one of the search result displayed
+	Then  The product price is displayed
+
+	
+	
